3cc5928ead5e4affa5c8d8607d94b7136c560f05
NickTsalyn <nicktsalyn@gmail.com>
2023-10-12 21:14:54 -0400

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,14 +17,14 @@ export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        {/* <Route path="/" element={<Layout />}> */}
           <Route index element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:movieId" element={<MovieDetails />}>
             <Route path="/movies/:movieId/cast" element={<MovieCast />} />
             <Route path="/movies/:movieId/reviews" element={<MovieReview />} />
           </Route>
-        </Route>
+        {/* </Route> */}
       </Routes>
     </div>
   );
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
+import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { Container } from './Layout.styled';
-import { Outlet } from 'react-router-dom';
 import { NavBar } from '../NavBar/NavBar';
 
 export const Layout = () => {
