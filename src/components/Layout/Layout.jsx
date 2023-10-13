f75a7b1f8daa5b8823bfe0c3fa97ea61afa1f5bf
NickTsalyn <nicktsalyn@gmail.com>
2023-10-12 21:39:13 -0400

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { Container } from './Layout.styled';
-import { NavBar } from 'components/NavBar/NavBar';
+// import { NavBar } from 'components/NavBar/NavBar';
 
 export const Layout = () => {
   return (
     <>
-    <NavBar/>
+    {/* <NavBar/> */}
       <main>
         <Container>
           <Suspense fallback={null}>
