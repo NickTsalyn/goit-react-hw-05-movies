4e684e52bc161a928c6f8de42c46af9cef675865
NickTsalyn <nicktsalyn@gmail.com>
2023-10-13 13:26:20 -0400

    Revert "update"
    This reverts commit 0ca264cfbc70bd8b6e7531426e9753ec38ce673c.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/goit-react-hw-05-movies'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
